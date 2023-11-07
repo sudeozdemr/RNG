67bbaeb7dad49288d3ac744ce0a56dd2fd539a86
sudeozdemir <sudezdmr@hotmail.com>
2023-11-07 07:30:23 +0300

    Değişiklikler.

--- DataCom/datacom.py
+++ DataCom/datacom.py
@@ -1,19 +1,19 @@
 import csv
 import random
 
-# CSV dosyasından verileri oku
+
 veriler = []
 with open('data_com_list.csv', 'r') as dosya:
     csv_okuyucu = csv.reader(dosya)
     for satir in csv_okuyucu:
         veriler.append(satir)
 
-# Verileri rastgele sırayla karıştır
+
 random.shuffle(veriler)
 
-# Döngü içinde verileri göster
+
 for veri in veriler:
-    print(*veri)  # * operatörü ile köşeli parantezleri ve tek tırnakları çıkarır
+    print(*veri)  
     tus = input("  ")
     if tus == 'q':
         break
--- OS/yeni.py
+++ OS/yeni.py
@@ -1,19 +1,17 @@
 import csv
 import random
 
-# CSV dosyasından verileri oku
+
 veriler = []
 with open('ornek.csv', 'r') as dosya:
     csv_okuyucu = csv.reader(dosya)
     for satir in csv_okuyucu:
         veriler.append(satir)
 
-# Verileri rastgele sırayla karıştır
 random.shuffle(veriler)
 
-# Döngü içinde verileri göster
 for veri in veriler:
-    print(*veri, sep='')  # Boşluğu kaldırmak için sep='' kullanılır
+    print(*veri, sep='')  
     tus = input(" ")
     if tus == 'q':
         break
