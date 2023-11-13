f9131cb65a7abb2b462d2be19185861f8ac087e8
Sude ÖZDEMİR <sudezdmr@hotmail.com>
2023-11-13 17:11:17 +0300

    13.11.2023_changes.

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import csv
 import random
+import sys
 
 try:
-   
-    file_path = input("Lütfen bir CSV dosyası yolunu girin: ")
+
+    file_path = sys.argv[1]
 
     with open(file_path, 'r', newline='', encoding='utf-8') as file:
         csv_reader = csv.reader(file)
-        
         datas = [line for line in csv_reader]
 
     random.shuffle(datas)
 
     for data in datas:
-        print(*data, sep='', end='')  
-        tus = input("")
-        if tus.lower() == 'q':
+        print(*data, end='')  
+        sude = input("")
+        if sude == '0':
             break
 
+
 except FileNotFoundError:
-    print(f"Dosya bulunamadı: {file_path}. Lütfen geçerli bir dosya yolunu kontrol edin.")
-except Exception as eror:
-    print(f"Bir hata oluştu: {eror}") 
+    print(f"File not found: {file_path}. Please enter a valid file path.")
+except Exception as error:
+    print(f"An error occurred: {error}")
